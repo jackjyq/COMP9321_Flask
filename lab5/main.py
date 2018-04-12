d4bca1cf2ab364418c6f093f8af133e05db04174
Jack Jiang <jackjyq@outlook.com>
2018-04-12 12:06:55 +1000

    start with lab5

---
+++ lab5/main.py
@@ -0,0 +1,40 @@
+from collections import deque
+from flask import Flask
+import datetime
+import threading
+import time
+import psutil as psutil
+from jsonpickle import json
+
+app = Flask(__name__)
+mem_history = deque(maxlen=60)
+
+'''
+ This function records the memory usage of the system every ten seconds. 
+ The function logs the amount of memory used through the system and the time. 
+'''
+
+def record_mem():
+    while True:
+        if len(mem_history) == 60:
+            mem_history.pop()
+        date = datetime.datetime.now()
+        date = int(date.timestamp())
+        mem_history.append({'date': date, 'memory_usage': psutil.virtual_memory().percent})
+        time.sleep(10)
+
+
+t = threading.Thread(target=record_mem)
+t.daemon = True
+t.start()
+
+
+@app.route("/mem", methods=['GET'])
+def get_memory_history():
+    history = list(mem_history)
+    # to allow external access to the endpoint you need to add 'Access-Control-Allow-Origin': '*' to the headers
+    return json.dumps(history), 200, {'Access-Control-Allow-Origin': '*'}
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
