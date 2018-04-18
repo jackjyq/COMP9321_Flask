6ccb1ae891c4807ea8d2fab6b15a98cea6beefbc
Jack Jiang <jackjyq@outlook.com>
2018-04-18 21:26:43 +1000

    remove venv for better performance

---
+++ ass2/README.txt
@@ -0,0 +1,5 @@
+Student ID: z5129432
+Name: Yiqun Jiang 
+Installation Guide:
+- How to compile, deploy the service code (step-by-step) 
+- Specify if there is anything else you want to mention to help the assessor
---
+++ ass2/server/main.py
@@ -0,0 +1,10 @@
+import requests
+
+BOCSAR = "http://resource.mcndsj.com/lga/"
+AUPOST = """https://docs.google.com/spreadsheets/d/1tHCxouhyM4edDvF60VG7\
+            nzs5QxID3ADwr3DGJh71qFg/edit#gid=900781287"""
+
+
+
+
+
